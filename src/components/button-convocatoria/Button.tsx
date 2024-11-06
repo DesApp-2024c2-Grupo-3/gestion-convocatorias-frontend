cfad9b301de17c22c952122d618d639e45139778
PalNahu <nahuelmartin.palma@estudiantes.unahur.edu.ar>
2024-11-06 15:15:30 -0300

    Ventana emergente para info de convocatorias  con modal y useState
    Participantes:
    
    Nicolás Arcamone,
    Mateo Burella Barco,
    Franco Casasola,
    Nahuel Martín Palma,
    Francisco Renó.

--- src/components/button-convocatoria/Button.tsx
+++ src/components/button-convocatoria/Button.tsx
@@ -4,13 +4,18 @@ import './buttons.css'
 
 interface Props {
     nombre: string;
-    className: string;
+    className?: string;
     iconoDelBoton?: React.ReactNode;
+    onClick?: () => void;
 }
 
 
-const ButtonConvocatoria: React.FC<Props> = ({nombre, className, iconoDelBoton}) => {
-    return <Button variant="primary" className={className}>{nombre} {iconoDelBoton}</Button>
+const ButtonConvocatoria: React.FC<Props> = ({nombre, className, iconoDelBoton, onClick}) => {
+    return (
+      <Button onClick={onClick} variant="primary" className={className}>
+        {nombre} {iconoDelBoton}
+      </Button>
+    );
 }
 
 export default ButtonConvocatoria
