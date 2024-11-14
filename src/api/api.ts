ea1d153ab2fc3efcd938dc6408c985e89c58fa35
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2024-11-14 16:12:35 -0300

    Se corrigio el Post de convocatorias

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,13 @@ import { InformacionGeneralValues } from "../pages/FormNuevaConvocatoria/schemas
 import { CamposValues } from "../pages/FormNuevaConvocatoria/FormPages/FormFormato";
 import axios from "axios";
 
-export const postConvocatoria = (data: { informacionGeneral: InformacionGeneralValues, formato: CamposValues}) => {
+export const postConvocatoria = (informacionGeneral: InformacionGeneralValues, formato: CamposValues) => {
+    
+    const data = {
+        informacionGeneral,
+        formato: formato.campos
+    } 
+    
     axios.post('http://localhost:3000/convocatorias', data)
     .then(function(response) {
         console.log(response)
