5c98646d12f33903bc6c8fbe0f13b81998428471
CasasolaFranco <franco.casasola@estudiantes.unahur.edu.ar>
2025-03-29 12:01:43 -0300

    Se agrego un modal para visualizar los campos de un formato en la seccion formatos

--- src/pages/Formatos/components/FormatoCard.tsx
+++ src/pages/Formatos/components/FormatoCard.tsx
@@ -1,30 +1,35 @@
 import { Card, CardActionArea, CardContent } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { FormatoProps } from "../Formatos";
+import FormatoDialog from "./FormatoDialog";
 
-interface FormatoCardProps {
-    idFormato: string;
-    nombreDelFormato: string;
-    campos: any[]
-}
+const FormatoCard = ({ _id, nombreDelFormato, campos }: FormatoProps) => {
+
+    const [showDialog, setShowDialog] = useState(false)
 
 
-const FormatoCard = ({ idFormato, nombreDelFormato, campos }: FormatoCardProps) => {
     return (
-        <Card
-        sx={{
-            borderTop: 15,
-            borderTopColor: "#56A42C",
-            width: {
-                xs: '25rem',
-                md: '500px'
-            }
-        }}>
-            <CardActionArea>
-                <CardContent>
-                    <h5>{nombreDelFormato}</h5>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <>
+            <Card
+                sx={{
+                    borderTop: 15,
+                    borderTopColor: "#56A42C",
+                    width: '20rem'
+                }}>
+                <CardActionArea
+                    onClick={() => setShowDialog(true)}
+                >
+                    <CardContent>
+                        <h5>{nombreDelFormato}</h5>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+
+            <FormatoDialog
+                formatoData={{_id, nombreDelFormato, campos}}
+                showDialogState={{showDialog, setShowDialog}}
+            />
+        </>
     )
 }
 
