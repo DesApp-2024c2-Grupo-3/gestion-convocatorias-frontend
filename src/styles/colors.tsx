12a6d01b233940a66365722bed71b335b8b901bc
nicolas.delafuente <ni.delafuente@gmail.com>
2025-07-07 23:32:48 -0300

    chore: constante de colores

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -2,6 +2,26 @@
 export const primaryColor = "#56A42C";
 export const secondaryColor = "#06A3C9";
 export const white = "#ffffff";
-export const gray100 = "#fafaff";
 export const black = "#000000";
 export const red = "#ff0000";
+export const gray100 = "#fafaff";
+
+
+export const COLOR_PRIMARY = "#06A3C9";
+export const COLOR_PRIMARY_HOVER = "#048AAE";
+export const COLOR_SECONDARY = "#56A42C";
+export const COLOR_SECONDARY_HOVER = "#417c21";
+export const COLOR_DANGER = "#a22c29";
+export const COLOR_DANGER_HOVER = "#8b211e";
+
+export const WHITE = "#ffffff";
+export const GRAY_100 = "#fafaff";
+export const GRAY_200 = "#f5f5f5";
+export const GRAY_300 = "#e0e0e0";
+export const GRAY_400 = "#bdbdbd";
+export const GRAY_500 = "#9e9e9e";
+export const GRAY_600 = "#757575";
+export const GRAY_700 = "#616161";
+export const GRAY_800 = "#424242";
+export const GRAY_900 = "#212121";
+export const BLACK = "#000000";
